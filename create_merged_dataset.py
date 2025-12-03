35c74bff8038b537c0ba90aeff3cb4b573794d3b
Okimoka <wonthewarp@googlemail.com>
2025-12-03 13:17:30 +0100

    Refactor dataset directory structure
    Updated directory structure for eye-tracking and EEG data.

--- create_merged_dataset.py
+++ create_merged_dataset.py
@@ -9,33 +9,45 @@ Expects the following folder structure:
 DATASET/
 ├─ ET/                                # Eye-Tracking
 │  ├─ NDARAB977GFB/
-│  │  ├─ txt/
-│  │  │  ├─ NDARAB977GFB_Video-DM_Events.txt
-│  │  │  ├─ NDARAB977GFB_Video-DM_Samples.txt
-│  │  │  └─ ...                       # all et files for participant
-│  │  └─ ...                          # optionally, tsv or idf folders
+│  │  ├─ Eyetracking/
+│  │  │  ├─ txt/
+│  │  │  │  ├─ NDARAB977GFB_Video-DM_Events.txt
+│  │  │  │  ├─ NDARAB977GFB_Video-DM_Samples.txt
+│  │  │  │  └─ ...                   # all ET .txt files for participant
+│  │  │  └─ ...                      # optionally, tsv or idf folders
+│  │  ├─ Behavioral/
+│  │  │  └─ ...                      # any behavioral files for participant
+│  │  └─ ...
 │  ├─ <id>/
-│  │  └─ txt/
-│  │     └─ ...
+│  │  ├─ Eyetracking/
+│  │  │  └─ txt/
+│  │  │     └─ ...
+│  │  ├─ Behavioral/
+│  │  │  └─ ...
+│  │  └─ ...
 │  ├─ <id>/
-│  │  └─ txt/
-│  │     └─ ...
-│  └─ ...                             # for all subjects
+│  │  ├─ Eyetracking/
+│  │  │  └─ txt/
+│  │  │     └─ ...
+│  │  ├─ Behavioral/
+│  │  │  └─ ...
+│  │  └─ ...
+│  └─ ...                            # for all subjects
 │
 └─ EEG/                               # 11 BIDS releases (randomly named)
-   ├─ release1_any_name/
-   │  ├─ dataset_description.json
-   │  ├─ participants.tsv
-   │  ├─ sub-NDARAB977GFB/
-   │  │  └─ eeg/
-   │  │     ├─ sub-NDARAB977GFB_task-ThePresent_eeg.set
-   │  │     ├─ sub-NDARAB977GFB_task-ThePresent_eeg.json
-   │  │     ├─ sub-NDARAB977GFB_task-ThePresent_channels.tsv
-   │  │     └─ sub-NDARAB977GFB_task-ThePresent_events.tsv
-   │  │     └─ ...
-   │  └─ ...                          # for all subjects in this release
-   └─ release2/
-      └─ ...
+├─ release1_any_name/
+│  ├─ dataset_description.json
+│  ├─ participants.tsv
+│  ├─ sub-NDARAB977GFB/
+│  │  └─ eeg/
+│  │     ├─ sub-NDARAB977GFB_task-ThePresent_eeg.set
+│  │     ├─ sub-NDARAB977GFB_task-ThePresent_eeg.json
+│  │     ├─ sub-NDARAB977GFB_task-ThePresent_channels.tsv
+│  │     ├─ sub-NDARAB977GFB_task-ThePresent_events.tsv
+│  │     └─ ...
+│  └─ ...                         # for all subjects in this release
+└─ release2/
+└─ ...
 
 """
 
